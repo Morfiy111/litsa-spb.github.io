8d311e0a83ec713f68023190347fc83cd7355563
morfiy <keks46@yandex.ru>
2020-07-23 11:21:25 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -195,3 +195,15 @@ owl.owlCarousel({
     autoplayTimeout: 3000,
     autoplayHoverPause: true
 });
+
+var owl = $('.feedback-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+});
